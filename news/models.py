0af044509878dfd9c04b179957daf67e33750f81
min <minotodor@gmail.com>
2020-03-17 15:04:00 +0300

    first commit with app News

---
+++ news/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    published_date = models.DateTimeField()
+    #rating =
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+ 
